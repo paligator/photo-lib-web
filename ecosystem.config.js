ed5d3f34fb70df4f43a3892876f552dfaae7fe47
pavol.zelenka <pavol.zelenka@itera.no>
2019-08-24 15:40:32 +0200

    pm2 deployment

---
+++ ecosystem.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+	apps: [{
+		name: 'photo-lib-api',
+		script: './build/App.js',
+		args: '',
+		instances: '2',
+		autorestart: true,
+		max_restarts: 5,
+		watch: false,
+		max_memory_restart: '500M',
+		merge_logs: true,
+		env: {
+			NODE_ENV: 'development'
+		},
+		env_production: {
+			NODE_ENV: 'production'
+		}
+	}],
+
+	deploy: {
+		production: {
+			user: 'photolib',
+			host: '192.168.1.11',
+			key: '/c/Users/pavol.zelenka/.ssh/paligator.pem',
+			ref: 'origin/testbuild',
+			repo: 'git@github.com:paligator/photo-lib-web.git',
+			path: '/home/photolib/photo-lib/photo-lib-web',
+			'post-deploy': 'npm run build:prod && serve -s build && pm2 reload ecosystem.config.js --env production'
+		}
+	}
+};
