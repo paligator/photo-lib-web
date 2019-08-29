bad78ceb9a9fd6b53d3d30513e1aabec78857d36
pavol.zelenka <pavol.zelenka@itera.no>
2019-08-29 08:15:40 +0200

    fix branch to deploy production

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -22,7 +22,7 @@ module.exports = {
 			user: 'photolib',
 			host: '192.168.1.11',
 			key: '/c/Users/pavol.zelenka/.ssh/paligator.pem',
-			ref: 'origin/testbuild',
+			ref: 'origin/master',
 			repo: 'git@github.com:paligator/photo-lib-web.git',
 			path: '/home/photolib/photo-lib/photo-lib-web',
 			'post-deploy': 'npm install && npm run build:prod && pm2 reload ecosystem.config.js --env production'
