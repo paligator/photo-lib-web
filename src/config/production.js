240d8e55d4ae7556678221c8a50b0fdf87aee7a7
pavol.zelenka <pavol.zelenka@itera.no>
2019-09-08 12:28:48 +0200

    Small fixies

--- src/config/production.js
+++ src/config/production.js
@@ -1,5 +1,5 @@
 export default {
 	"apiUrl": "https://photo-lib-api.paligator.sk:6005",
 	"imageProxyUrl": "https://photo-lib-api.paligator.sk:6005",
-	"apiGraphQLUrl": "https://photo-lib-api.paligator.sk:6005"
+	"apiGraphQLUrl": "https://photo-lib-api.paligator.sk:6005/graphql"
 };
