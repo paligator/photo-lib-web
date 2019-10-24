35213da3abc214f48a5a722bcc1444e349f9b2b5
pavol.zelenka <pavol.zelenka@itera.no>
2019-10-24 07:39:37 +0200

    Remove multi mode for photo Tags

--- src/components/basics/PhotoTags.js
+++ src/components/basics/PhotoTags.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { Button } from 'reactstrap';
-import * as C from "../../api/Common";
 
 class PhotoTags extends Component {
 
@@ -8,7 +7,6 @@ class PhotoTags extends Component {
 		super(props);
 		this.onClick = this.onClick.bind(this);
 		this.getBtnClasses = this.getBtnClasses.bind(this);
-		this.getSelectedTags = this.getSelectedTags.bind(this);
 	}
 
 	render() {
@@ -18,11 +16,6 @@ class PhotoTags extends Component {
 				<Button data-tag="nice" className={this.getBtnClasses("nice")} onClick={this.onClick} disabled={this.props.disabled}>Nice</Button>
 				<Button data-tag="top" className={this.getBtnClasses("top")} onClick={this.onClick} disabled={this.props.disabled}>TOP</Button>
 				<Button data-tag="boring" className={this.getBtnClasses("boring")} onClick={this.onClick} disabled={this.props.disabled}>boring</Button>
-
-				{(this.props.showNotTaggedTag === true) ?
-					(<Button data-tag="notTagged" className={this.getBtnClasses("notTagged")} onClick={this.onClick}>No Tag</Button>) : (null)
-				}
-
 			</div>
 		);
 	}
@@ -39,16 +32,10 @@ class PhotoTags extends Component {
 		const removeTags = [];
 		const addTags = [];
 
-		// in single mode we deselect all active tags
-		if (this.props.mode === 'single') {
-			activeTagBtns.forEach(btn => {
-				removeTags.push(btn.dataset.tag);
-				btn.classList.remove("active");
-			});
-		} else {
-			// in multi mode only clicked btn
-			clickedBtn.classList.remove("active");
-		}
+		activeTagBtns.forEach(btn => {
+			removeTags.push(btn.dataset.tag);
+			btn.classList.remove("active");
+		});
 
 		// mark clicked tag as active
 		if (isActive === false) {
@@ -60,30 +47,11 @@ class PhotoTags extends Component {
 		if (this.props.updateTags) {
 			this.props.updateTags({ variables: { albumId: this.props.albumId, photoName: this.props.photoName, addTags, removeTags } });
 		}
-
-		//save active tags into cookies
-		if (this.props.saveSelected === true) {
-			const tags = this.getSelectedTags();
-			C.setCookie(this.props, 'selectedFilterTags', JSON.stringify(tags));
-			//this.props.updatePhotoFilterTags(tags);
-		}
-	}
-
-	getSelectedTags() {
-		const activeTagBtns = this.node.querySelectorAll('.active');
-		const tags = [];
-		activeTagBtns.forEach(btn => {
-			tags.push(btn.dataset.tag);
-		});
-		return tags;
 	}
 }
 
 PhotoTags.defaultProps = {
-	mode: "single",
 	tags: [],
-	showNotTagged: false,
-	saveSelected: false,
 	disabled: false,
 };
 
