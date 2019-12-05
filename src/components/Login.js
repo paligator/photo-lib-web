1baefec49f795d34a84384bee6467a20b7e34720
paligator <paligator@gmail.com>
2019-12-05 09:48:26 +0100

    Login class reworked to function

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,74 +1,50 @@
-import React, { Component } from 'react';
-import { connect } from "react-redux";
+import React, { useState, useRef } from 'react';
 import { Form, FormGroup, Label, Input, Button } from 'reactstrap';
 import * as actions from "../constants/action-types";
 
-class Login extends Component {
+import { useSelector, useDispatch } from "react-redux";
 
-	constructor(props) {
-		super(props);
-		this.doLogin = this.doLogin.bind(this);
-		this.state = { wrongLogin: this.props.wrongLogin };
+const Login = () => {
+
+	const node = useRef();
+	const dispatch = useDispatch();
+
+	const [hideWrongLoginMsg, setHideWrongLoginMsg] = useState("dependsOnRedux");
+	const reduxUnSuccessfulLogin = useSelector(state => {	
+		return state.unSuccessfulLogin || false;
+	});
+
+	function hideWrongLogin() {
+		setHideWrongLoginMsg("yes");
 	}
 
-	static getDerivedStateFromProps(props, state) {
-
-		if (state.source === "hide") {
-			return { wrongLogin: false, source: "" }
-		}
-
-		if (props.wrongLogin === state.wrongLogin) {
-			return null;
-		} else {
-			return { wrongLogin: props.wrongLogin, source: "" }
-		}
-	}
-
-	doLogin(event) {
+	function doLogin(event) {
 		event.preventDefault();
-		
-		const email = this.node.querySelector("#txtEmail").value;
-		const password = this.node.querySelector("#txtPassword").value;
 
-		this.props.onLogin(email, password);
+		const email = node.current.querySelector("#txtEmail").value;
+		const password = node.current.querySelector("#txtPassword").value;
+
+		setHideWrongLoginMsg("dependsOnRedux");
+		dispatch({ type: actions.LOGIN, payload: { email, password } });
 	}
 
-	render() {
-		return (
-			<div ref={node => this.node = node} className="flexContainer" style={{ width: "100%" }}>
-				<Form style={{ width: "20em", paddingTop: "10vh" }} onSubmit={this.doLogin}>
-					<FormGroup>
-						<Label for="txtEmail">Email</Label>
-						<Input type="email" name="email" id="txtEmail" placeholder="guest@paligator.sk" onChange={(() => this.hideWrongLogin())} />
-					</FormGroup>
-					<FormGroup>
-						<Label for="txtPassword">Password</Label>
-						<Input type="password" name="password" id="txtPassword" placeholder="Password123" onChange={(() => this.hideWrongLogin())} />
-					</FormGroup>
-					<FormGroup style={{textAlign: "center"}}><Button className="btn-primary">Login</Button></FormGroup>
-					{(this.state.wrongLogin === true) ? <FormGroup><div className="alert alert-danger">Wrong email or password!</div></FormGroup> : (null)}
-				</Form>
-			</div >
-		)
-	}
-
-	hideWrongLogin() {
-		this.setState({ wrongLogin: false, source: "hide" });
-	}
+	return (
+		<div ref={node} className="flexContainer" style={{ width: "100%" }}>
+			<Form style={{ width: "20em", paddingTop: "10vh" }} onSubmit={doLogin}>
+				<FormGroup>
+					<Label for="txtEmail">Email</Label>
+					<Input type="email" name="email" id="txtEmail" placeholder="guest@paligator.sk" required onChange={(() => hideWrongLogin())} />
+				</FormGroup>
+				<FormGroup>
+					<Label for="txtPassword">Password</Label>
+					<Input type="password" name="password" id="txtPassword" placeholder="Password123" required onChange={(() => hideWrongLogin())} />
+				</FormGroup>
+				<FormGroup style={{ textAlign: "center" }}><Button className="btn-primary">Login</Button></FormGroup>
+				{(hideWrongLoginMsg === "dependsOnRedux" && reduxUnSuccessfulLogin === true) ? <FormGroup><div className="alert alert-danger">Wrong email or password!</div></FormGroup> : (null)}
+			</Form>
+		</div >
+	);
 }
 
-function mapStateToProps(state) {
-	return {
-		wrongLogin: state.unSuccessfulLogin
-	};
-}
+export default Login;
 
-function mapDispatchToProps(dispatch) {
-	return {
-		onLogin: (email, password) => {
-			dispatch({ type: actions.LOGIN, payload: { email, password } });
-		},
-	}
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(Login);
