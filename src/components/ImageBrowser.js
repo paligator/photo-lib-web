e42d3e13bcfa58dc034c048d15e8c831f18a41f4
paligator <paligator@gmail.com>
2020-01-02 09:01:06 +0100

    Optimize browser for small screens

--- src/components/ImageBrowser.js
+++ src/components/ImageBrowser.js
@@ -112,35 +112,88 @@ class ImageBrowser extends Component {
 
     const isBigScreen = this.props.styles.isBigScreen === true;
 
-    return (
 
-      <div id="divBrowswerTop" ref={node => this.node = node} className="row" style={{ height: 'calc(100% - 80px)', maxWidth: "100%" }} >
+    if (isBigScreen === true) {
+      return (
+        <div id="divBrowswerTop" ref={node => this.node = node} className="row" style={{ height: 'calc(100% - 80px)', maxWidth: "100%" }} >
+          <GlobalHotKeys keyMap={this.globalKeyMap} handlers={this.globalKeyHandlers} />
 
-        <GlobalHotKeys keyMap={this.globalKeyMap} handlers={this.globalKeyHandlers} />
-
-        {(isBigScreen === true) ?
           <div className="col-sm-2" style={{ display: "flex", flexDirection: "column", height: "100%", overflow: "auto", overflowX: "hidden" }} >
             <AlbumInfo />
             <ImagesFilter cookies={this.props.cookies} />
             <ImageDetails markThumbWithTag={this.markThumbWithTag} />
             <Comments />
           </div>
-          : null
-        }
 
-        <div className={isBigScreen ? "col-sm-10" : "col-sm-12"} style={{ padding: "0" }}>
+          <div className="col-sm-10" style={{ padding: "0" }}>
+
+            {(albumIsReady === true && isReloadingPhotos === false) ? (
+
+              <div id="mainMe" className="column" style={{ width: "100%", height: "100%" }}>
+                {(photoCount === 0) ?
+                  (
+                    <Warning text="Sorry, there are no photos for this filter!" icon="fas fa-eye-slash" />
+                  )
+                  :
+                  (
+                    <Swipeable onSwipedLeft={() => { this.nextPhoto(true) }} onSwipedRight={() => { this.nextPhoto(false) }} >
+                      <div className="row" style={{ height: '100%', width: "100%", position: "absolute" }}>
+                        <div className="column" style={{ width: "100%" }}>
+                          <div id="divPhotoLoaders" key="divPhotoLoaders" className="photoLoadersDiv">
+                            <PhotoLoader id="loaderIn" imgId="imgPhotoIn" className={fadeInClass} display="block" key={photoUrlFadeIn || "photoIn"} photoName={photoNameFadeIn} photoUrl={photoUrlFadeIn} onAnimationEnd={(e) => this.onAnimationEnd(e)} />
+                            <PhotoLoader id="loaderOut" imgId="imgPhotoOut" className={fadeOutClass} display="block" key={photoUrlFadeOut || "photoOut"} photoName={photoNameFadeOut} photoUrl={photoUrlFadeOut} onAnimationEnd={(e) => this.onAnimationEnd(e)} />
+                            <PhotoLoader id="loaderBuffer" imgId="imgBimgPhotoBuffer" display="none" key={photoUrlBuffer} photoName={photoNameBuffer} photoUrl={photoUrlBuffer} />
+                          </div>
+                          <Suspense fallback={<div>Loading...</div>}>
+                            <Thumbs urlPath={album.path} markThumbAsSelected={this.markThumbAsSelected} setNextFading={this.setNextFading} style={{ width: "100%" }} />
+                          </Suspense>
+
+                          {(isBigScreen === false) ?
+                            <div style={{ padding: "10px" }}>
+                              <Comments />
+                            </div>
+                            : null
+                          }
+                        </div>
+
+                        <button id="btnPrevPhoto" className="btn-nav btn-nav-left fas" aria-label="Previous photo" tooltip="Previous photo" onClick={() => { this.nextPhoto(false) }}></button>
+                        <button id="btnNextPhoto" className="btn-nav btn-nav-right fas" aria-label="Next photo" tooltip="Next photo" onClick={() => { this.nextPhoto(true) }}></button>
+                        <button className="btn-fullscreen fas" aria-label="Go to Fullscreen" tooltip="Go to Fullscreen" onClick={() => { this.goToFullScreen() }}></button>
+
+                        <div className="photoCounter">{selectedPhotoIndex + 1}/{photoCount}</div>
+
+                        <p id="divExif" className="exif animated" style={{ display: "none" }}></p>
+                        <div id="btnExif" className="btn-exif" onClick={this.showExif}>EXIF</div>
+
+                      </div>
+                    </Swipeable>
+                  )}
+              </div>
+            ) :
+              this.getLoadingDiv()
+            }
+
+          </div>
+        </div >
+      );
+    }
+
+    return (
+      <div id="divBrowswerTop" ref={node => this.node = node} className="row" style={{ maxWidth: "100%" }} >
+
+        <GlobalHotKeys keyMap={this.globalKeyMap} handlers={this.globalKeyHandlers} />
+
+        <div style={{ width: "100%" }}>
 
           {(albumIsReady === true && isReloadingPhotos === false) ? (
 
-            <div id="mainMe" className="column" style={{ width: "100%", height: "100%" }}>
+            <div id="mainMe" className="column" style={{ width: "100%" }}>
 
-              {(isBigScreen === false) ? (
-                <div style={{ padding: "10px" }}>
-                  <AlbumInfo />
-                  <ImagesFilter cookies={this.props.cookies} />
-                  <ImageDetails markThumbWithTag={this.markThumbWithTag} />
-                </div>) : null
-              }
+              <div style={{padding: "5px"}}>
+                <AlbumInfo />
+                <ImagesFilter cookies={this.props.cookies} />
+                <ImageDetails markThumbWithTag={this.markThumbWithTag} />
+              </div>
 
               {(photoCount === 0) ?
                 (
@@ -149,42 +202,39 @@ class ImageBrowser extends Component {
                 :
                 (
                   <Swipeable onSwipedLeft={() => { this.nextPhoto(true) }} onSwipedRight={() => { this.nextPhoto(false) }} >
-                    <div className="row" style={{ height: '100%', width: "100%", position: "absolute" }}>
+                    <div style={{ width: "100%" }}>
                       <div className="column" style={{ width: "100%" }}>
                         <div id="divPhotoLoaders" key="divPhotoLoaders" className="photoLoadersDiv">
-                          <PhotoLoader id="loaderIn" imgId="imgPhotoIn" className={fadeInClass} display="block" key={photoUrlFadeIn || "photoIn"} photoName={photoNameFadeIn} photoUrl={photoUrlFadeIn} onAnimationEnd={(e) => this.onAnimationEnd(e)} />
-                          <PhotoLoader id="loaderOut" imgId="imgPhotoOut" className={fadeOutClass} display="block" key={photoUrlFadeOut || "photoOut"} photoName={photoNameFadeOut} photoUrl={photoUrlFadeOut} onAnimationEnd={(e) => this.onAnimationEnd(e)} />
+                          <PhotoLoader id="loaderIn" imgId="imgPhotoIn" className={fadeInClass + " sm"} display="block" key={photoUrlFadeIn || "photoIn"} photoName={photoNameFadeIn} photoUrl={photoUrlFadeIn} onAnimationEnd={(e) => this.onAnimationEnd(e)} />
+                          <PhotoLoader id="loaderOut" imgId="imgPhotoOut" className={fadeOutClass + " sm"} display="block" key={photoUrlFadeOut || "photoOut"} photoName={photoNameFadeOut} photoUrl={photoUrlFadeOut} onAnimationEnd={(e) => this.onAnimationEnd(e)} />
                           <PhotoLoader id="loaderBuffer" imgId="imgBimgPhotoBuffer" display="none" key={photoUrlBuffer} photoName={photoNameBuffer} photoUrl={photoUrlBuffer} />
+
+                          <button id="btnPrevPhoto" className="btn-nav sm btn-nav-left fas" aria-label="Previous photo" tooltip="Previous photo" onClick={() => { this.nextPhoto(false) }}></button>
+                          <button id="btnNextPhoto" className="btn-nav sm btn-nav-right fas" aria-label="Next photo" tooltip="Next photo" onClick={() => { this.nextPhoto(true) }}></button>
+                          <button className="btn-fullscreen sm fas" aria-label="Go to Fullscreen" tooltip="Go to Fullscreen" onClick={() => { this.goToFullScreen() }}></button>
+
+                          <div className="photoCounter">{selectedPhotoIndex + 1}/{photoCount}</div>
+
+                          <p id="divExif" className="exif sm animated" style={{ display: "none" }}></p>
+                          <div id="btnExif" className="btn-exif sm" onClick={this.showExif}>EXIF</div>
+
                         </div>
+
                         <Suspense fallback={<div>Loading...</div>}>
                           <Thumbs urlPath={album.path} markThumbAsSelected={this.markThumbAsSelected} setNextFading={this.setNextFading} style={{ width: "100%" }} />
                         </Suspense>
 
-                        {(isBigScreen === false) ?
-                          <div style={{ padding: "10px" }}>
-                            <Comments />
-                          </div>
-                          : null
-                        }
+                        <div style={{padding: "5px"}}>
+                          <Comments />
+                        </div>
+
                       </div>
-
-                      <button id="btnPrevPhoto" className="btn-nav btn-nav-left fas" aria-label="Previous photo" tooltip="Previous photo" onClick={() => { this.nextPhoto(false) }}></button>
-                      <button id="btnNextPhoto" className="btn-nav btn-nav-right fas" aria-label="Next photo" tooltip="Next photo" onClick={() => { this.nextPhoto(true) }}></button>
-                      <button className="btn-fullscreen fas" aria-label="Go to Fullscreen" tooltip="Go to Fullscreen" onClick={() => { this.goToFullScreen() }}></button>
-
-                      <div className="photoCounter">{selectedPhotoIndex + 1}/{photoCount}</div>
-
-                      <p id="divExif" className="exif animated" style={{ display: "none" }}></p>
-                      <div id="btnExif" className="btn-exif" onClick={this.showExif}>EXIF</div>
-
                     </div>
                   </Swipeable>
                 )}
             </div>
-
-
           ) :
-            this.getLoadingDiv()
+            this.getLoadingDiv("25%")
           }
 
         </div>
@@ -362,10 +412,15 @@ class ImageBrowser extends Component {
 
     target = this.findDisplayedTagComponent(target);
 
+    if (!target) {
+      console.log("Target shouldn't be null");
+      return;
+    }
+
     target.classList.add("thumbSelected");
     if (scroolTo === true) {
       //FIXME: I don't fucking understand when I use global Hotkeys (rith, left) and scroll behaviour = "smooth" scrollIntoView doesn't work, with hotkey I have to use "auto" or "instant"
-      target.scrollIntoView({ behavior: scrollbehavior || "smooth", inline: "center", block: "center" });
+      target.scrollIntoView({ behavior: scrollbehavior || "smooth", inline: "center", block: "nearest" });
     }
     this.disableNavButtons();
   }
@@ -381,41 +436,41 @@ class ImageBrowser extends Component {
     }
   }
 
-  getLoadingDiv() {
-    return (< div style={{ position: "absolute", left: "37.5%", top: "15%", height: "auto", width: "25%" }}>
-      <ReactLoading type='spin' color="red" style={{ width: "100%", height: "100%" }} />
-    </div>)
+  getLoadingDiv(top) {
+    return (< div style={{ position: "absolute", left: "37.5%", top: top || "15%", height: "auto", width: "25%"}}>
+  <ReactLoading type='spin' color="red" style={{ width: "100%", height: "100%" }} />
+    </div >)
   }
 
-  goToFullScreen() {
-    const isInFullScreen = (document.fullscreenElement && document.fullscreenElement !== null) ||
-      (document.webkitFullscreenElement && document.webkitFullscreenElement !== null) ||
-      (document.mozFullScreenElement && document.mozFullScreenElement !== null) ||
-      (document.msFullscreenElement && document.msFullscreenElement !== null);
+goToFullScreen() {
+  const isInFullScreen = (document.fullscreenElement && document.fullscreenElement !== null) ||
+    (document.webkitFullscreenElement && document.webkitFullscreenElement !== null) ||
+    (document.mozFullScreenElement && document.mozFullScreenElement !== null) ||
+    (document.msFullscreenElement && document.msFullscreenElement !== null);
 
-    const docElm = this.node.querySelector("#divPhotoLoaders");
-    if (!isInFullScreen) {
-      if (docElm.requestFullscreen) {
-        docElm.requestFullscreen();
-      } else if (docElm.mozRequestFullScreen) {
-        docElm.mozRequestFullScreen();
-      } else if (docElm.webkitRequestFullScreen) {
-        docElm.webkitRequestFullScreen();
-      } else if (docElm.msRequestFullscreen) {
-        docElm.msRequestFullscreen();
-      }
-    } else {
-      if (document.exitFullscreen) {
-        document.exitFullscreen();
-      } else if (document.webkitExitFullscreen) {
-        document.webkitExitFullscreen();
-      } else if (document.mozCancelFullScreen) {
-        document.mozCancelFullScreen();
-      } else if (document.msExitFullscreen) {
-        document.msExitFullscreen();
-      }
+  const docElm = this.node.querySelector("#divPhotoLoaders");
+  if (!isInFullScreen) {
+    if (docElm.requestFullscreen) {
+      docElm.requestFullscreen();
+    } else if (docElm.mozRequestFullScreen) {
+      docElm.mozRequestFullScreen();
+    } else if (docElm.webkitRequestFullScreen) {
+      docElm.webkitRequestFullScreen();
+    } else if (docElm.msRequestFullscreen) {
+      docElm.msRequestFullscreen();
+    }
+  } else {
+    if (document.exitFullscreen) {
+      document.exitFullscreen();
+    } else if (document.webkitExitFullscreen) {
+      document.webkitExitFullscreen();
+    } else if (document.mozCancelFullScreen) {
+      document.mozCancelFullScreen();
+    } else if (document.msExitFullscreen) {
+      document.msExitFullscreen();
     }
   }
+}
 }
 
 function mapStateToProps(state, ownState) {
